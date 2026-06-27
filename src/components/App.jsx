e0dd4279c55fd54c662f5e9b7bff03007b540af4
bugra-git <237065860+bugra-git@users.noreply.github.com>
2026-06-28 02:00:11 +0300

    Have app component returun header, main, footer and fetch data

---
+++ src/components/App.jsx
@@ -0,0 +1,56 @@
+import "../styles/App.css";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [pokemonData, setPokemonData] = useState([]);
+
+  useEffect(() => {
+    async function fetchPokemon() {
+      try {
+        const response = await fetch(
+          "https://pokeapi.co/api/v2/pokemon?limit=151",
+        );
+
+        const data = await response.json();
+
+        const pokemonDetails = await Promise.all(
+          data.results.map(async (pokemon) => {
+            const detailResponse = await fetch(pokemon.url);
+            const detailData = await detailResponse.json();
+
+            return {
+              id: detailData.id,
+              name: detailData.name,
+              image: detailData.sprites.other.dream_world.front_default,
+              types: detailData.types.map((typeInfo) => typeInfo.type.name),
+            };
+          }),
+        );
+
+        setPokemonData(pokemonDetails);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchPokemon();
+  }, []);
+
+  return (
+    <>
+      <header>
+        <h1>Memory Card Game</h1>
+        <p>
+          Get points by clicking on an image but don't click on any more than
+          once!
+        </p>
+      </header>
+      <main></main>
+      <footer>
+        <p>&copy; 2026 Memory Card Game. All rights reserved.</p>
+      </footer>
+    </>
+  );
+}
+
+export default App;
